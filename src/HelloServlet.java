fd3b567c07f0264996bc8282d19ea4c623982414
wulin <2583277062@qq.com>
2017-10-11 21:38:22 +0800

    first

---
+++ src/HelloServlet.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+public class HelloServlet extends HttpServlet{
+  
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+          
+        try {
+            response.getWriter().println("<h1>Hello Servlet!</h1>");
+            response.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+     @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	// TODO Auto-generated method stub
+    	super.doPost(req, resp);
+    } 
+}
